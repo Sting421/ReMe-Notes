72337b5a043466ccdf8fd6ec690d4211abbcc79e
jivstuban <larsjib@gmail.com>
2025-09-18 23:09:37 +0800

    adding backend using postgres

---
+++ Backend/src/test/java/com/ReMe/ReMe/ReMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReMe.ReMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
